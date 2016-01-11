d04c05ac2060a724b62f1b5cd5a6e15ef10b2c3a
Andres Kirschbaum <andres@parsimotion.com>
2016-01-11 02:09:01 -0300

    ItemsController

---
+++ Ejercicio/Controllers/ItemsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Ejercicio.Models;
+using Ejercicio.Services;
+
+namespace Ejercicio.Controllers
+{
+    public class ItemsController : ApiController
+    {
+	    private readonly MercadolibreItemsClient itemsClient;
+	    public ItemsController(MercadolibreItemsClient itemsClient)
+	    {
+		    this.itemsClient = itemsClient;
+	    }
+
+	    // GET: api/Items
+        public IEnumerable<Item> Get(string query)
+        {
+            return itemsClient.Search(query);
+        }
+    }
+}
